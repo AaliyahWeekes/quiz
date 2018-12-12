cf236f9b23678146c3abf3dea971e6010592ab27
Aaliyah Weekes <weekesaaliyah1@gmail.com>
2018-12-12 22:52:20 +0000

    buzzfeed quiz

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
+        $("#question1").click(function(){
+            
+        })
+        
+        
+        
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
